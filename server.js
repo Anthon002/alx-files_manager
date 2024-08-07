c26a999691d9fcd393a3c2377574739c8488729e
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-07 16:47:13 +0100

    task 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
+import express from 'express';
+import injectRoutes from './routes';
+import startServer from './libs/boot';
 
 const server = express();
 
