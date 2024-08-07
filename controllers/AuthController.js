d10f5a1668eb149e629b8ab8ae9c485732ea81a0
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-07 18:35:28 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,19 @@
-/* eslint-disable import/no-named-as-default */
-import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class AuthController {
-  static async getConnect(req, res) {
-    const { user } = req;
-    const token = uuidv4();
+  static async getConnect(request_, response_) {
+    const { user } = request_;
+    const randomToken = uuidv4();
 
-    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-    res.status(200).json({ token });
+    await redisClient.set(`auth_${randomToken}`, user._id.toString(), 24 * 60 * 60);
+    response_.status(200).json({ randomToken });
   }
 
-  static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
+  static async getDisconnect(request_, response_) {
+    const randomToken = request_.headers['x-randomToken'];
 
-    await redisClient.del(`auth_${token}`);
-    res.status(204).send();
+    await redisClient.del(`auth_${randomToken}`);
+    response_.status(204).send();
   }
 }
